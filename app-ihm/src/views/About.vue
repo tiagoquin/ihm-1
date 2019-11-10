e8926e5e039a49512b9bdb88ac6298ac5b196379
chadanlo <tiago.quin@gmail.com>
2019-11-10 17:50:02 +0100

    add download button

--- app-ihm/src/views/About.vue
+++ app-ihm/src/views/About.vue
@@ -1,10 +1,18 @@
 <template>
   <!--  -->
-  <v-row align="start" justify="center">
-    <v-col >
-        <h2>Step 1: Crop your picture</h2>
-        <vue-cropper ref="cropper" :src="imgSrc" alt="Source Image" :containerStyle="style"></vue-cropper>
-        <!--
+  <v-row
+    align="start"
+    justify="center"
+  >
+    <v-col>
+      <h2>Step 1: Crop your picture</h2>
+      <vue-cropper
+        ref="cropper"
+        :src="imgSrc"
+        alt="Source Image"
+        :containerStyle="style"
+      ></vue-cropper>
+      <!--
         <v-img
           :src="somevalue"
           class="grey lighten-2"
@@ -15,71 +23,111 @@
         />
         -->
     </v-col>
-        <v-col >
-          <form>
-            <h2>Step 2: Get your data</h2>
-            <p>
-      <v-btn
-        color="pink"
-        class="mr-4"
-        @click="getData()"
-      >
-      Obtain
-      </v-btn>
-            </p>
+    <v-col>
+      <form>
+        <h2>Step 2: Get your data</h2>
+        <p>
+          <v-btn
+            color="pink"
+            class="mr-4"
+            @click="getData()"
+          >
+            Obtain
+          </v-btn>
+        </p>
 
-          <v-text-field
-            label="Coordinate X"
-            outlined
-            :value=current.top
-          ></v-text-field>
-          <v-text-field
-            label="Coordinate Y"
-            :value=current.left
-            outlined
-          ></v-text-field>
-          <v-text-field
-            label="Width (Size X)"
-            :value=current.width
-            outlined
-          ></v-text-field>
-          <v-text-field
-            label="Height (Size Y)"
-            :value=current.height
-            outlined
-          ></v-text-field>
-          <v-text-field
-            label="Label"
-            v-model=labelvalue
-            outlined
-          ></v-text-field>
-          </form>
+        <v-text-field
+          label="Coordinate X"
+          outlined
+          v-model=current.top
+          type=number
+          @blur="setData()"
+        ></v-text-field>
+        <v-text-field
+          label="Coordinate Y"
+          v-model=current.left
+          outlined
+          @blur="setData()"
+          type=number
+        ></v-text-field>
+        <v-text-field
+          label="Width (Size X)"
+          v-model=current.width
+          outlined
+          @blur="setData()"
+          type="number"
+        ></v-text-field>
+        <v-text-field
+          label="Height (Size Y)"
+          v-model=current.height
+          outlined
+          @blur="setData()"
+          type="number"
+        ></v-text-field>
+        <v-text-field
+          label="Label"
+          v-model=labelvalue
+          @blur="setData()"
+          outlined
+        ></v-text-field>
+      </form>
 
       <h2>Step 3: Add it to the list</h2>
-        <p>
-      <v-btn
-        color="pink"
-        class="mr-4"
-        @click="pushData()"
-      >
-      Push
-      </v-btn>
-            </p>
-        </v-col>
+      <p>
+        <v-btn
+          color="blue"
+          class="mr-4"
+          @click="pushData()"
+        >
+          Push
+        </v-btn>
+      </p>
+
+      <h2>Step 3: Add it to the list</h2>
+      <p>
+        <v-btn
+          color="blue"
+          class="mr-4"
+          @click="setData()"
+        >
+          Set
+        </v-btn>
+      </p>
+    </v-col>
     <!-- List -->
-    <v-col >
-      <a @click.prevent="getData">getData</a>
+    <v-col>
+
+     <h2>Step 5: Download your list</h2>
+      <p>
+        <v-btn
+          color="green"
+          class="mr-4"
+          :href="thefile"
+          download="coordinates.json"
+        >
+          Download
+        </v-btn>
+      </p>
 
       <v-list>
         <v-list-item-group v-model="model">
-          <v-list-item v-for="elem in coordonnees" :key="elem.id">
+          <v-list-item
+            v-for="elem in coordonnees"
+            :key="elem.id"
+          >
             <v-list-item-icon>
-              <v-icon large v-text="icon" @click="deleteElem(elem.id)" color="red darken-2" />
+              <v-icon
+                large
+                v-text="icon"
+                @click="deleteElem(elem.id)"
+                color="red darken-2"
+              />
             </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title v-text="elem.obj.text3" />
-              <v-list-item-title v-text="elem.obj.text1" />
-              <v-list-item-title v-text="elem.obj.text2" />
+            <v-list-item-content @click="retrieveData(elem.obj)">
+              <span>id: {{elem.id}}</span>
+              <h3>label: {{elem.obj.label}}</h3>
+              <span>x: <b>{{elem.obj.top}}</b> y: <b>{{elem.obj.left}}</b></span>
+              <span>width: <b>{{elem.obj.width}}</b> height: <b>{{elem.obj.height}}</b></span>
             </v-list-item-content>
           </v-list-item>
         </v-list-item-group>
@@ -95,7 +143,7 @@ import "cropperjs/dist/cropper.css";
 
 export default {
   name: "about",
-  data() {
+  data () {
     return {
       model: 1,
       icon: "mdi-delete-forever",
@@ -105,7 +153,7 @@ export default {
       current: {
         top: 0,
         left: 0,
-        height:0,
+        height: 0,
         width: 0,
       },
       labelvalue: '',
@@ -119,25 +167,53 @@ export default {
   props: {
     somevalue: String
   },
-  created: function() {
+  created: function () {
     this.imgSrc = this.somevalue;
   },
-  mounted: function() {},
+  mounted: function () { },
   components: { VueCropper },
+  computed: {
+    thefile() {
+      console.log(this.coordonnees);
+      return `data:json/octet-stream;charset=utf-8,${JSON.stringify(this.coordonnees, null, 2)}`
+    },
+  },
   methods: {
-    getData() {
+    getData () {
       this.data = this.$refs.cropper.getCropBoxData();
-      this.current= this.data;
+      this.current = this.data;
       // console.log(this.data);
     },
-    pushData(){
-      const text1 = `x: ${this.current.top}, y: ${this.current.left}`;
-      const text2 = `width: ${this.current.width}, height: ${this.current.height}`;
-      const text3 = `${this.labelvalue}`;
-      this.coordonnees.push({ id: this.lastId++, obj: { text3, text1, text2} });
+    pushData () {
+      this.coordonnees.push({
+        id: this.lastId++,
+        obj: {
+          top: this.current.top,
+          left: this.current.left,
+          width: this.current.width,
+          height: this.current.height,
+          label: this.labelvalue
+        }
+      });
     },
-    deleteElem(id){
-      console.log('delete', id);
+    setData () {
+      console.log(this.current);
+
+      this.$refs.cropper.setCropBoxData({
+        top: Number(this.current.top), left: Number(this.current.left), width: Number(this.current.width), height: Number(this.current.height)
+      });
+    },
+    retrieveData (obj) {
+      this.current = {
+        top: obj.top,
+        left: obj.left,
+        height: obj.height,
+        width: obj.width,
+      };
+      this.labelvalue = obj.label;
+      this.setData();
+    },
+    deleteElem (id) {
       this.coordonnees = this.coordonnees.filter(obj => obj.id !== id);
     }
   }
